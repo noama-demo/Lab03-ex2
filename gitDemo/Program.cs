602507bb6e545fa5994fa94d9f6aa863e6104e9f
Noam Amrani <noama@sela.co.il>
2020-09-22 07:52:22 +0300

    Add hello-world solution and project

---
+++ gitDemo/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gitDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world");
+
+
+            ClassA ca = new ClassA();
+            ClassB cb = new ClassB();
+
+        }
+    }
+}
